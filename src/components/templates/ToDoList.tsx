f9080accd13d9cc2fdb3ae1521c683b080ec501e
Benjamin Pearse <benjamin.pearse@bt.com>
2024-12-24 13:44:19 +0000

    feat: allow to do items to be marked as complete

--- src/components/templates/ToDoList.tsx
+++ src/components/templates/ToDoList.tsx
@@ -3,6 +3,8 @@
 import React, { SyntheticEvent } from "react";
 import { IToDoItem } from "@/types/todoList";
 import ToDoForm from "../organisms/ToDoForm";
+import ToDoItem from "../molecules/ToDoItem";
+import { v4 as uuidv4 } from "uuid";
 
 const ToDoList = () => {
   const [toDoItems, setToDoItems] = React.useState<IToDoItem[]>([]);
@@ -22,21 +24,38 @@ const ToDoList = () => {
       const newItem: IToDoItem = {
         name: newToDoName,
         complete: false,
+        id: uuidv4(),
       };
 
       // Add new to do item to existing list of to do items
       setToDoItems((prevToDoItems) => [...prevToDoItems, newItem]);
 
       (document.getElementById("to-do-input") as HTMLInputElement).value = "";
+      console.log("Added item: ", newItem);
     }
+  };
+
+  const handleToDoCompletionToggle = (checked: boolean, id: string) => {
+    const tempToDoItems = toDoItems;
+    const itemToUpdate = tempToDoItems.findIndex((item) => item.id === id);
+    tempToDoItems[itemToUpdate].complete = checked;
+    setToDoItems(tempToDoItems);
+    console.log("Updated item: ", itemToUpdate);
   };
 
   return (
     <div className="container mx-auto h-screen p-4 border max-w-4xl flex flex-col gap-2">
       <h1>ToDoList</h1>
       <ToDoForm submitEventHandler={handleAddToDoItem} />
-      {toDoItems.map((item, index) => (
-        <div key={index}>{item.name}</div>
+      {toDoItems.map((item) => (
+        <div key={item.id}>
+          <ToDoItem
+            id={item.id}
+            name={item.name}
+            complete={item.complete}
+            handleCompletionChange={handleToDoCompletionToggle}
+          />
+        </div>
       ))}
     </div>
   );
--- src/types/todoList.ts
+++ src/types/todoList.ts
@@ -1,4 +1,5 @@
 export interface IToDoItem {
   name: string;
   complete: boolean;
+  id: string;
 }
