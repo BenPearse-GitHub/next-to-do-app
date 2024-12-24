745dd6bc58db6b047e6bd1737d32466fc3a59adf
Benjamin Pearse <benjamin.pearse@bt.com>
2024-12-24 13:44:04 +0000

    feat: add ToDoItem component

---
+++ src/components/molecules/ToDoItem.tsx
@@ -0,0 +1,27 @@
+import { IToDoItem } from "@/types/todoList";
+import React from "react";
+import { Checkbox } from "../ui/checkbox";
+
+interface ToDoItemProps extends IToDoItem {
+  handleCompletionChange: (checked: boolean, name: string) => void;
+}
+
+const ToDoItem = ({
+  id,
+  name,
+  complete,
+  handleCompletionChange,
+}: ToDoItemProps) => {
+  return (
+    <div className="flex flex-row gap-2 align-middle">
+      <Checkbox
+        id={id}
+        defaultChecked={complete}
+        onCheckedChange={() => handleCompletionChange(!complete, id)}
+      />
+      <label htmlFor={id}>{name}</label>
+    </div>
+  );
+};
+
+export default ToDoItem;
