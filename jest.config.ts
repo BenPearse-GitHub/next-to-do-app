b331cee080a4df7e2f32eb651fa297d2f28e1422
Benjamin Pearse <benjamin.pearse@bt.com>
2025-02-07 16:01:41 +0000

    chore: additional jest config

--- jest.config.ts
+++ jest.config.ts
@@ -11,10 +11,11 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     // ...
     "^@/(.*)$": "<rootDir>/src/$1",
+    "^@/components/(.*)$": "<rootDir>/components/$1",
   },
 };
 
